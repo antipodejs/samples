960aaa80719d110e564583f1faaaa854b415a0ec
Serge Alischuk <ut7cq@yahoo.com>
2016-05-05 17:18:44 +0300

    sample 1

--- src/app/components/main.js
+++ src/app/components/main.js
@@ -19,9 +19,21 @@ new $a.Component({
 console.log('configModules = ', configModules);
 
 const collection = new $a.Collection();
-collection.initial(configModules);
 
-console.log('collection = ', collection);
+$a.xhr.getJSON(
+        'http://localhost:3005/records/0/1000000',
+        //'http://localhost:3005/children/0',
+        function(response) {
+          //this.Indexer[id] = response;
+          //this.expandTree(this.Indexer[id], after);
+          let r = response.forEach((i)=>{ i.title = i.name});
+          console.log('response = ', response);
+          collection.initial(response);
+          /*Collection.initial(response);
+          infoBlock.set('total', response.length);
+          infoBlock.render();*/
+
+
 
 const list = new $a.iList({
 	collection,
@@ -35,6 +47,26 @@ const list = new $a.iList({
 })
 .render();
 
+
+        }.bind(this)
+      );
+
+//collection.initial(configModules);
+
+console.log('collection = ', collection);
+/*
+const list = new $a.iList({
+	collection,
+	directive: 'smplist',
+	data: {
+		numItems: 13 // Note: visible items is equal: numItems - 3
+	},
+	components: [
+		{name: 'item', kind: $a.Component, template: '<div style="position: absolute; height: 20px; transform: translate3d(0,0,0);">{{title}}</div>'}
+	]
+})
+.render();
+*/
 console.log('list = ', list);
 
 export default {};
